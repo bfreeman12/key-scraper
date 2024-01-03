9ce2ce2b731e75ba7b10f3150d80c773e34fc1d1
Brandon <99849502+bfreeman12@users.noreply.github.com>
2024-01-03 13:05:24 +0100

    fixed multipage key issue

--- fanaticalContent.js
+++ fanaticalContent.js
@@ -1,21 +1,21 @@
 if (typeof gamesCodes === "undefined") {
-  var gamesCodes = document.querySelectorAll(".order-item-details-container");
-  var allGameData = [];
+  let gamesCodes = document.querySelectorAll(".order-item-details-container");
+  let allGameData = [];
+
+  gamesCodes.forEach((game) => {
+    let nameElement = game.querySelector(".game-name");
+    let keyElement = game.querySelector(".key-input-field");
+
+    if (nameElement && keyElement) {
+      let name = nameElement.innerText;
+      let key = keyElement.value;
+
+      allGameData.push({ name: name, key: key });
+    }
+  });
+
+  chrome.runtime.sendMessage({
+    event: "gameData",
+    gameData: allGameData,
+  });
 }
-
-gamesCodes.forEach((game) => {
-  let nameElement = game.querySelector(".game-name");
-  let keyElement = game.querySelector(".key-input-field");
-
-  if (nameElement && keyElement) {
-    let name = nameElement.innerText;
-    let key = keyElement.value;
-
-    allGameData.push({ name: name, key: key });
-  }
-});
-
-chrome.runtime.sendMessage({
-  event: "gameData",
-  gameData: allGameData,
-});
--- humbleContent.js
+++ humbleContent.js
@@ -1,21 +1,21 @@
 if (typeof gamesCodes === "undefined") {
-  var gamesCodes = document.querySelectorAll(".key-redeemer");
-  var allGameData = [];
+  let gamesCodes = document.querySelectorAll(".key-redeemer");
+  let allGameData = [];
+
+  gamesCodes.forEach((game) => {
+    let nameElement = game.querySelector(".heading-text h4");
+    let keyElement = game.querySelector(".js-keyfield");
+
+    if (nameElement && keyElement) {
+      let name = nameElement.innerText;
+      let key = keyElement.getAttribute("title");
+
+      allGameData.push({ name: name, key: key });
+    }
+  });
+
+  chrome.runtime.sendMessage({
+    event: "gameData",
+    gameData: allGameData,
+  });
 }
-
-gamesCodes.forEach((game) => {
-  let nameElement = game.querySelector(".heading-text h4");
-  let keyElement = game.querySelector(".js-keyfield");
-
-  if (nameElement && keyElement) {
-    let name = nameElement.innerText;
-    let key = keyElement.getAttribute("title");
-
-    allGameData.push({ name: name, key: key });
-  }
-});
-
-chrome.runtime.sendMessage({
-  event: "gameData",
-  gameData: allGameData,
-});
